6be61824958a851f27da879e032bd707788778df
Shanthilal K <shanthilal12062005@gmail.com>
2026-02-25 19:28:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//seletcing button popupoverlay popupbox
+
+var popupoverlay = document.querySelector(".popup-overlay");
+var popupbox = document.querySelector(".popup-box");
+var addpopupbutton = document.getElementById("add-popup-button");
+
+addpopupbutton.addEventListener("click", function() {
+    popupoverlay.style.display = "block";
+    popupbox.style.display = "block";
+})
+
+//select canecl button
+
+var cancelpopup = document.getElementById("cancel-popup")
+var popupoverlay = document.querySelector(".popup-overlay");
+var popupbox = document.querySelector(".popup-box");
+
+cancelpopup.addEventListener("click", function(event){
+    event.preventDefault();
+    popupoverlay.style.display = "none";
+    popupbox.style.display = "none";
+})
+
+//select container add-book book-title-input book-author-input book-description-input delete button
+
+var conatiner =document.querySelector(".container");
+var addbook =document.getElementById("add-book");
+var booktitleinput = document.getElementById("book-title-input");
+var bookauthorinput = document.getElementById("book-author-input");
+var bookdescriptioninput = document.getElementById("book-description-input");
+var deletebookbutton = document.getElementById("delete-book-button");
+
+addbook.addEventListener("click", function(event){
+    event.preventDefault();
+    var div = document.createElement("div");
+    div.setAttribute("class", "book-container");
+    div.innerHTML = `<h2>${booktitleinput.value}</h2>
+    <h4>${bookauthorinput.value}</h4>
+    <p>${bookdescriptioninput.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`;
+    conatiner.append(div)
+    popupoverlay.style.display = "none";
+    popupbox.style.display = "none";
+})
+
+function deletebook(event){
+
+    event.target.parentElement.remove();
+}
